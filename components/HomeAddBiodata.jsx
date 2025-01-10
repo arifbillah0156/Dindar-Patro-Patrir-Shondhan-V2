a9dee20bbe1476bc7ed65df8a73313d3c445af4b
arifbillah0156 <arifbillah0156@gmail.com>
2025-01-10 20:24:05 +0600

    plusBtnEdit

--- components/HomeAddBiodata.jsx
+++ components/HomeAddBiodata.jsx
@@ -7,12 +7,12 @@ export default function HomeAddBiodata() {
   return (
     <div className="mt-12 flex justify-center">
       <div
-        className="text-center w-[max-content] my-5"
+        className="text-center w-full sm:w-[max-content] my-5 px-4"
         title="ফ্রিতে আপনার বায়োডাটা তৈরি করুন।"
       >
         <Link
           href={"/create_new_biodata"}
-          className="text-2xl md:text-4xl bg-gradient-to-r from-purple-500 via-pink-500 to-rose-500 text-white  px-10 py-8 rounded-full shadow-lg shadow-rose-300"
+          className="text-2xl sm:text-4xl bg-gradient-to-r from-purple-500 via-pink-500 to-rose-500 text-white px-12 py-8 rounded-full shadow-lg shadow-rose-300 block"
         >
           + বায়োডাটা তৈরি করুন
         </Link>
