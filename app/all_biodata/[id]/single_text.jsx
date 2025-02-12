777324d4956379fd45ed59e684fa55486ba7ba5c
arifbillah0156 <arifbillah0156@gmail.com>
2025-02-12 19:48:59 +0600

    edit54fv44a4v4vvvf

--- app/all_biodata/[id]/single_text.jsx
+++ app/all_biodata/[id]/single_text.jsx
@@ -1,10 +1,10 @@
 export default function Text({ text, value }) {
   return (
     <div className="flex text-start">
-      <p className="min-w-[40%] font-semibold px-1 border-gray-300 py-3  pb-4 md:text-lg">
+      <p className="min-w-[40%] text-gray-900 font-semibold px-1 border-gray-300 py-3  pb-4 md:text-lg">
         {text}{" "}
       </p>
-      <p className="min-w-[60%] px-1 pl-2 py-3  border-gray-300  pb-4  md:text-lg">
+      <p className="min-w-[60%] text-gray-900 px-1 pl-2 py-3  border-gray-300  pb-4  md:text-lg">
         {"↔ "} {value}{" "}
       </p>
     </div>
